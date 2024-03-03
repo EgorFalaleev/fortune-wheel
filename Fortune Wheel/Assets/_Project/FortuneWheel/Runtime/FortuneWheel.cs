24404c210eec5b50d0f2b2d28d0666fd1ea9cfe7
EgorF <egor1268@gmail.com>
2024-03-03 12:08:33 +0500

    implemented wheel numbers generation

---
+++ Fortune Wheel/Assets/_Project/FortuneWheel/Runtime/FortuneWheel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+namespace _Project.Scripts.Runtime
+{
+    public class FortuneWheel : MonoBehaviour
+    {
+        private WheelData _wheelData;
+
+        private void Start()
+        {
+            _wheelData = new WheelData(12);
+            _wheelData.GenerateWheelNumbers();
+
+            foreach (var number in _wheelData.WheelNumbers)
+            {
+                Debug.Log($"{number} ");
+            }
+        }
+    }
+
+    public class WheelData
+    {
+        public HashSet<int> WheelNumbers { get; }
+
+        private int _wheelSize;
+        private Random _random;
+
+        public WheelData(int wheelSize)
+        {
+            _wheelSize = wheelSize;
+            WheelNumbers = new HashSet<int>();
+            _random = new Random();
+        }
+        
+        public void GenerateWheelNumbers()
+        {
+            WheelNumbers.Clear();
+            
+            while (WheelNumbers.Count != _wheelSize)
+            {
+                var nextNumber = _random.Next(1, 21) * 5;
+
+                WheelNumbers.Add(nextNumber);
+            }
+        }
+    }
+}
